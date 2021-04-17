88d84df5740c869d1d1c8944cebc6063b93baec9
Jérôme <git@jeromecst.com>
2021-04-17 22:03:45 +0200

    option to change color implemented

--- speedread.h
+++ speedread.h
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <time.h>
-#include <signal.h>
 #include "utf8.h"
 
 #define SIZE 100
@@ -16,14 +9,13 @@ struct Word {
 };
 
 int msleep(unsigned long msec);
-unsigned len(char *c);
-void copy(char *a, char *b);
-void add_str(char *s, unsigned n, unsigned add);
 unsigned size_bytes(char *s);
-unsigned display_word(char *s, unsigned *midword);
+void display_word(char *s, unsigned *midword);
 void clear_lines(unsigned a);
 void display_cursor();
 void display_tab();
 void get_mid_screen();
 void display(char *s, double add);
 void sig_handler(int signum);
+const unsigned long hash(const char *str);
+void switch_colors(char *c, char *col);
