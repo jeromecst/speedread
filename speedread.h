9b0462e71a14063ff64799d3d7bc17b240b2d9b9
Jérôme <git@jeromecst.com>
2021-08-17 16:07:39 +0200

    making code clearer

--- speedread.h
+++ speedread.h
@@ -10,13 +10,14 @@ struct Word {
 
 int msleep(unsigned long msec);
 unsigned size_bytes(char *s);
-void display_word(char *s, unsigned *midword);
+void display_word(char *word, unsigned *midword);
 void clear_lines(unsigned a);
 void display_cursor();
 void display_tab();
 void get_mid_screen();
-void display(char *s, double add);
+void read_word(char *word, double time_add);
 void sig_handler(int signum);
 const unsigned long hash(const char *str);
 void switch_colors(char *c);
 void manage_input();
+void add_str(char *str, unsigned str_size, unsigned add);
